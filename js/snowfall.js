b1ea5f22bc57e08b0df7debe008d8d1202ec3b78
Mohammadhawawdeh <mohammadhawawdeh@hotmail.com>
2023-12-20 10:39:46 +0300

    snow

--- js/snowfall.js
+++ js/snowfall.js
@@ -3,23 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function animateSnowflakes() {
         snowflakes.forEach(function (snowflake) {
-            const fallSpeed = parseFloat(snowflake.getAttribute('data-fallspeed')) || 2.0; // Adjust the fall speed as needed
-            const rotateSpeed = parseFloat(snowflake.getAttribute('data-rotatespeed')) || 0.5; // Adjust the rotation speed as needed
+            const fallSpeed = parseFloat(snowflake.getAttribute('data-fallspeed')) || 2.0;
+            const rotateSpeed = (Math.random() * 1) + 0.5; // Random rotation speed between 0.5 and 1.5
 
-            // Move the snowflake vertically
             const translateY = snowflake.offsetTop + fallSpeed;
             snowflake.style.transform = `translateY(${translateY}px) rotate(${rotateSpeed}deg)`;
 
-            // Reset the snowflake position when it goes below the viewport
             if (translateY > window.innerHeight) {
                 snowflake.style.transform = `translateY(-20px) rotate(${rotateSpeed}deg)`;
             }
         });
 
-        // Repeat the animation
         requestAnimationFrame(animateSnowflakes);
     }
 
-    // Start the animation
     animateSnowflakes();
 });
